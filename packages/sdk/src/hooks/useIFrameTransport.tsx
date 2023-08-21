0c3d1111a032bde3fd12f9deb776961ad041ec7f
Roman Roibu <roman@roibu.xyz>
2023-08-21 11:40:51 +0200

    Add IFrameTransport helper class and useIFrameTransport hooks

---
+++ packages/sdk/src/hooks/useIFrameTransport.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { RefObject, useEffect, useRef } from "react";
+import { IFrameTransport } from "../utils/iframeTransport";
+
+interface UseIFrameTransportProps {
+  requestExecutor: (request: any) => Promise<any>;
+  sourceRef: RefObject<any>;
+  targetRef: RefObject<any>;
+}
+
+export function useIFrameTransport({ requestExecutor, sourceRef, targetRef }: UseIFrameTransportProps) {
+  const transportRef = useRef<IFrameTransport | undefined>();
+
+  useEffect(() => {
+    if (transportRef.current) transportRef.current.cleanup();
+
+    if (!sourceRef?.current) return;
+    if (!targetRef?.current) return;
+
+    transportRef.current = new IFrameTransport(requestExecutor, { eventSource: sourceRef.current, eventTarget: targetRef.current });
+    return () => transportRef.current?.cleanup();
+  }, [sourceRef?.current, targetRef?.current]);
+
+  return transportRef;
+}
