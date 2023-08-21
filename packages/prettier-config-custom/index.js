575f1840c427dd2730e7084498eedd84fb2e4e93
Pedro Filho <pedro@filho.me>
2023-08-21 10:05:43 +0200

    adding overhaul optimizations in

---
+++ packages/prettier-config-custom/index.js
@@ -0,0 +1,7 @@
+/** @type {import("prettier").Config} */
+const config = {
+  tailwindFunctions: ["cva", "cn"],
+  plugins: ["prettier-plugin-tailwindcss"],
+};
+
+export default config;
--- packages/sdk/src/utils/mode.ts
+++ packages/sdk/src/utils/mode.ts
@@ -1,6 +1,7 @@
 const mode = {
   REDIRECT: "redirect",
   POPUP: "popup",
+  EMBEDDED: "embedded",
 } as const;
 
 export { mode };
