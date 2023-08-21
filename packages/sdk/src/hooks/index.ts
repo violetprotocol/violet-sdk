131f607d10533dd35725f633df1b48aa6567043f
Roman Roibu <roman@roibu.xyz>
2023-08-21 11:41:20 +0200

    Add useWeb3ReactIFrameExecutor hook

--- packages/sdk/src/hooks/index.ts
+++ packages/sdk/src/hooks/index.ts
@@ -1,2 +1,3 @@
 export * from "./useViolet";
 export { useThirdwebIFrameWalletConfig } from "./useThirdwebIFrameWalletConfig";
+export { useWeb3ReactIFrameExecutor } from "./useWeb3ReactIFrameExecutor";
---
+++ packages/sdk/src/hooks/useWeb3ReactIFrameExecutor.ts
@@ -0,0 +1,36 @@
+"use client"
+
+import { useWeb3React } from "@web3-react/core";
+import { RefObject, useEffect, useRef } from "react";
+import { useIFrameTransport } from "./useIFrameTransport";
+
+interface UseIFrameExecutorProps {
+  sourceRef: RefObject<any>;
+  targetRef: RefObject<any>;
+}
+
+/**
+ * This should be used in the parent window to execute requests from the child window.
+ * 
+ * @param sourceRef - the parent `window` to which the child posts messages as requests
+ * @param targetRef - the child `window` (iframe) to which the parent posts messages as replies
+ */
+export function useWeb3ReactIFrameExecutor({ sourceRef, targetRef }: UseIFrameExecutorProps) {
+  const ref_ = useRef<any>();
+  const w3 = useWeb3React();
+
+  useEffect(() => {
+    ref_.current = w3.connector;
+  }, [sourceRef?.current, targetRef?.current]);
+
+  useIFrameTransport({
+    async requestExecutor(request) {
+      console.log(`[PARENT REQUEST EXECUTOR]: `, request);
+      const connector = ref_.current;
+      const provider = connector.customProvider || w3.connector.provider;
+      return provider.request(request);
+    },
+    sourceRef,
+    targetRef,
+  });
+}
